11e19cfbef04d730fd62239a218499f8fe7d0264
Togrul Garazade <togrulgarazade@gmail.com>
2023-03-27 21:15:45 +0400

    first commit

---
+++ assets/js/register.js
@@ -0,0 +1,167 @@
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+nextBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+submitBtn.addEventListener("click", function(){
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+  setTimeout(function(){
+    alert("Your Form Successfully Signed up");
+    location.reload();
+  },800);
+});
+
+prevBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+
+
+
+// Date of birth select
+
+const yearSelect = document.getElementById("year");
+const monthSelect = document.getElementById("month");
+const daySelect = document.getElementById("day");
+
+const months = ['January', 'February', 'March', 'April', 
+'May', 'June', 'July', 'August', 'September', 'October',
+'November', 'December'];
+
+//Months are always the same
+(function populateMonths(){
+    for(let i = 0; i < months.length; i++){
+        const option = document.createElement('option');
+        option.textContent = months[i];
+        monthSelect.appendChild(option);
+    }
+    monthSelect.value = "January";
+})();
+
+let previousDay;
+
+function populateDays(month){
+    //Delete all of the children of the day dropdown
+    //if they do exist
+    while(daySelect.firstChild){
+        daySelect.removeChild(daySelect.firstChild);
+    }
+    //Holds the number of days in the month
+    let dayNum;
+    //Get the current year
+    let year = yearSelect.value;
+
+    if(month === 'January' || month === 'March' || 
+    month === 'May' || month === 'July' || month === 'August' 
+    || month === 'October' || month === 'December') {
+        dayNum = 31;
+    } else if(month === 'April' || month === 'June' 
+    || month === 'September' || month === 'November') {
+        dayNum = 30;
+    }else{
+        //Check for a leap year
+        if(new Date(year, 1, 29).getMonth() === 1){
+            dayNum = 29;
+        }else{
+            dayNum = 28;
+        }
+    }
+    //Insert the correct days into the day <select>
+    for(let i = 1; i <= dayNum; i++){
+        const option = document.createElement("option");
+        option.textContent = i;
+        daySelect.appendChild(option);
+    }
+    if(previousDay){
+        daySelect.value = previousDay;
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 1;
+        }
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 2;
+        }
+        if(daySelect.value === ""){
+            daySelect.value = previousDay - 3;
+        }
+    }
+}
+
+function populateYears(){
+    //Get the current year as a number
+    let year = new Date().getFullYear();
+    //Make the previous 100 years be an option
+    for(let i = 0; i < 101; i++){
+        const option = document.createElement("option");
+        option.textContent = year - i;
+        yearSelect.appendChild(option);
+    }
+}
+
+populateDays(monthSelect.value);
+populateYears();
+
+yearSelect.onchange = function() {
+    populateDays(monthSelect.value);
+}
+monthSelect.onchange = function() {
+    populateDays(monthSelect.value);
+}
+daySelect.onchange = function() {
+    previousDay = daySelect.value;
+}
